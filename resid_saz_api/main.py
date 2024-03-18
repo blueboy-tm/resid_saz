04e8f92d6e55669e6506763862e041ef3756c397
Arshia Ihammi <arshiaihammi@gmail.com>
2024-03-18 09:18:55 +0300

    fix text direction

--- resid_saz_api/main.py
+++ resid_saz_api/main.py
@@ -1,10 +1,11 @@
 from PIL import Image, ImageFont, ImageDraw
-from fastapi.responses import StreamingResponse 
+from fastapi.responses import StreamingResponse
 import requests
 from io import BytesIO
 from fastapi import FastAPI, Body
 
-proxies={'http': "socks5://localhost:1080", "https": "socks5://localhost:1080"}
+proxies = {'http': "socks5://localhost:1080",
+           "https": "socks5://localhost:1080"}
 
 fanums = {
     '0': '۰',
@@ -19,17 +20,20 @@ fanums = {
     '9': '۹',
 }
 
+
 def eng2fa(number):
     eng = ''
     for i in number:
         eng += fanums.get(i, i)
     return eng
 
+
 def secureText(number: str):
     try:
         return f'{number[0:4]} {number[4:6]}** **** {number[12:]}'
     except:
         return ''
+
 
 class Receipt(object):
     def __init__(
@@ -51,44 +55,53 @@ class Receipt(object):
 
     def bold(self, font_size: int):
         return ImageFont.truetype('fonts/IranYekan-Bold.ttf', font_size)
-    
+
     def extra_bold(self, font_size: int):
         return ImageFont.truetype('fonts/IranYekan-ExtraBold.ttf', font_size)
 
     def light(self, font_size: int):
         return ImageFont.truetype('fonts/IranYekan-Light.ttf', font_size)
-    
+
     def medium(self, font_size: int):
         return ImageFont.truetype('fonts/IranYekan-Medium.ttf', font_size)
 
     def regular(self, font_size: int):
         return ImageFont.truetype('fonts/IranYekan-Regular.ttf', font_size)
-    
+
     def centerText(self, y: int, text: str, font=None, fill=None):
         _, _, w, h = self.draw.textbbox((0, 0), text, font=font)
-        self.draw.text(((self.W-w)/2, y), text, font=font, fill=fill)
+        self.draw.text(((self.W-w)/2, y), text, font=font,
+                       fill=fill, direction="rtl")
 
     def text(self, x, y: int, text: str, font=None, fill=None):
-        self.draw.text((x, y), text, font=font, fill=fill)
+        self.draw.text((x, y), text, font=font, fill=fill, direction="rtl")
 
     def make(self):
         self.centerText(210, self.receiver, font=self.bold(28), fill="#283130")
-        self.centerText(245, eng2fa(self.receiverCard), font=self.medium(26), fill='#7f9797')
-        self.centerText(300, eng2fa(f'{self.amount:,} ریال'), font=self.bold(52), fill='#283130')
-        self.text(30, 582, eng2fa(self.date), font=self.regular(30), fill=(41,42,44))
-        self.text(30, 672, eng2fa(self.sender), font=self.regular(30), fill=(41,42,44))
-        self.text(30, 762, 'کارت به کارت', font=self.regular(30), fill=(41,42,44))
-        self.text(30, 852, secureText(eng2fa(self.senderCard)), font=self.regular(30), fill=(41,42,44))
-        self.text(30, 942, eng2fa(self.code), font=self.regular(30), fill=(41,42,44))
+        self.centerText(245, eng2fa(self.receiverCard),
+                        font=self.medium(26), fill='#7f9797')
+        self.centerText(300, eng2fa(
+            f'{self.amount:,} ریال'), font=self.bold(52), fill='#283130')
+        self.text(30, 582, eng2fa(self.date),
+                  font=self.regular(30), fill=(41, 42, 44))
+        self.text(30, 672, eng2fa(self.sender),
+                  font=self.regular(30), fill=(41, 42, 44))
+        self.text(30, 762, 'کارت به کارت',
+                  font=self.regular(30), fill=(41, 42, 44))
+        self.text(30, 852, secureText(eng2fa(self.senderCard)),
+                  font=self.regular(30), fill=(41, 42, 44))
+        self.text(30, 942, eng2fa(self.code),
+                  font=self.regular(30), fill=(41, 42, 44))
         responce = requests.get(self.bankImage, proxies=None)
-        if responce.status_code != 200: return
+        if responce.status_code != 200:
+            return
         image_file = Image.open(BytesIO(responce.content)).convert('RGBA')
         image_file = image_file.resize((23, 23))
         self.base.paste(image_file, (305, 166), image_file)
 
-    
     def show(self):
         self.base.show()
+
 
 app = FastAPI()
 
@@ -101,7 +114,7 @@ async def root(data: dict = Body(...)):
         data.get('receiver', ''),
         data.get('receiver_card', ''),
         data.get('amount', 0),
-       data.get('time', '') + ' ' + data.get('date', ''),
+        data.get('time', '') + ' ' + data.get('date', ''),
         data.get('code', ''),
     )
     receipt.make()
